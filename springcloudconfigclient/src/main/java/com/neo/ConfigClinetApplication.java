a084cb763a4452d4f8d6fcbc541185220a6f13c0
Winnie.Chen <37991693+cyx441984694@users.noreply.github.com>
2021-01-30 11:04:09 +0800

    Add files via upload

---
+++ springcloudconfigclient/src/main/java/com/neo/ConfigClinetApplication.java
@@ -0,0 +1,13 @@
+package com.neo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ConfigClinetApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigClinetApplication.class,args);
+    }
+}
